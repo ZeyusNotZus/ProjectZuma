41c75f01ffcd7a748c383461e4be3d51fdf21d16
ZeyusNotZus <zbflorida@up.edu.ph>
2026-05-27 15:08:23 +0800

    Bullet Class and Player Shoot Implemented

--- entities.py
+++ entities.py
@@ -3,7 +3,7 @@ import math
 
 class Ally:
 
-    def __init__(self, x: int, y: int, w: int = 16, h: int = 16):
+    def __init__(self, x: float, y: float, w: int = 16, h: int = 16):
         self.x = x
         self.y = y
         self.w = w  # size of sprite is 16x16
@@ -17,7 +17,7 @@ class Ally:
 
 class Enemy:
 
-    def __init__(self, x: int, y: int, speed: float = 1.0, w: int=16, h: int =16):
+    def __init__(self, x: float, y: float, speed: float = 1.0, w: int = 16, h: int = 16):
         self.x = x
         self.y = y
         self.speed = speed
@@ -35,12 +35,46 @@ class Enemy:
     
     def move(self, direction: int): # direction is in degrees
         self.vx = self.speed * math.cos(math.radians(direction))
-        self.vy = self.speed * math.sin(math.radians(direction))
+        self.vy = -self.speed * math.sin(math.radians(direction))
+
+class Bullet:
+    def __init__(self, x: float, y: float, angle: float, speed: float = 3.0):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.w = 8
+        self.h = 8
+        
+        self.vx = self.speed * math.cos(angle)
+        self.vy = self.speed * math.sin(angle)
+
+    def update(self):
+        self.x += self.vx
+        self.y += self.vy
+
+    def draw(self):
+        pyxel.circ(self.x, self.y, 2, 8)
+
 
 class Player(Ally):
 
-    def __init__(self, x: int, y: int, w: int = 16, h: int = 16):
+    def __init__(self, x: float, y: float, w: int = 16, h: int = 16):
         super().__init__(x, y, w, h)
+    
+    def update(self, bullets_list: list):
+        super().update()
+
+        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+            player_center_x = self.x + self.w / 2
+            player_center_y = self.y + self.h / 2
+
+            target_x = pyxel.mouse_x
+            target_y = pyxel.mouse_y
+
+            angle = math.atan2(target_y - player_center_y, target_x - player_center_x)
+            new_bullet = Bullet(player_center_x, player_center_y, angle)
+
+            bullets_list.append(new_bullet)
 
     def draw(self):
         pyxel.blt(
@@ -48,14 +82,11 @@ class Player(Ally):
             0, 0,
             self.w, self.h, 0
         )
-    
-    def update(self):
-        ...
 
 class SimpleEnemy(Enemy):
 
-    def __init__(self, x: int, y: int, w: int = 16, h: int = 16):
-        super().__init__(x, y, w, h)
+    def __init__(self, x: float, y: float, speed: float = 1.0, w: int = 16, h: int = 16):
+        super().__init__(x, y, speed, w, h)
 
     def draw(self):
         pyxel.blt(
--- game.py
+++ game.py
@@ -7,16 +7,34 @@ SCREEN_HEIGHT: int = 120
 class Game:
     def __init__(self):
         self.exp = 0
+        self.player = entities.Player(SCREEN_WIDTH // 2 - 8, SCREEN_HEIGHT // 2 + 25)
+        self.enemies = []
+        self.bullets = []
         
-        self.player = entities.Player(SCREEN_WIDTH // 2 - 8, SCREEN_HEIGHT // 2 - 8)
-       
-        pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Project Z")
+        # pyxel
+        pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title = "Project Z")
+        pyxel.mouse(True)
         pyxel.load("sprites.pyxres")
         pyxel.run(self.update, self.draw)
-        
     
     def update(self):
-        self.player.update()
+        self.player.update(self.bullets)
+
+        if pyxel.frame_count % 60 == 0:
+            self.generate_enemy()
+
+        for enemy in self.enemies:
+            enemy.update()
+            enemy.move(0)
+
+        for bullet in self.bullets[:]:
+            bullet.update()
+
+            if (bullet.x < 0 or bullet.x > SCREEN_WIDTH or 
+                bullet.y < 0 or bullet.y > SCREEN_HEIGHT):
+                self.bullets.remove(bullet)
+            
+        self.check_collisions()
 
     def draw(self):
         pyxel.cls(0)
@@ -27,6 +45,31 @@ class Game:
         )
 
         self.player.draw()
+
+        for enemy in self.enemies:
+            enemy.draw()
+        
+        for bullet in self.bullets:
+            bullet.draw()
+    
+    def generate_enemy(self):
+        enemy = entities.SimpleEnemy(0, 25, 0.5)
+        self.enemies.append(enemy)
+    
+    def check_collisions(self):
+        for bullet in self.bullets[:]:
+            for enemy in self.enemies[:]:
+                if (enemy.x <= bullet.x <= enemy.x + enemy.w and
+                    enemy.y <= bullet.y <= enemy.y + enemy.h):
+                    
+                    if bullet in self.bullets:
+                        self.bullets.remove(bullet)
+                    if enemy in self.enemies:
+                        self.enemies.remove(enemy)
+                    
+                    self.exp += 1
+                    break
+
 
 def main():
     Game()
